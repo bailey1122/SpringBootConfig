f40d25859731ed86916105453977972752e43e40
Illya <illyashkurko112@gmail.com>
2021-01-06 16:50:20 +0200

    Initial commit: SpringBootConfig

--- src/main/java/com/pr/springbootconfig/GreetingController.java
+++ src/main/java/com/pr/springbootconfig/GreetingController.java
@@ -4,12 +4,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class GreetingController {
 
     // injects the value of the property into the variable
-    @Value("${my.greeting}")
+    @Value("${my.greeting: default value}")
     private String greetingMessage;
+
+    @Value("some static message")
+    private String staticMessage;
+
+    @Value("${my.list.values}")
+    private List<String> listValues;
 
     @GetMapping("/greeting")
     public String greeting() {
